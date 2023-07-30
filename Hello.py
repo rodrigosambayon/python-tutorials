bb060d6fd3ab05973061f1dcbd34e4850d7bf9f9
Rodrigo Sambayon <sambayonrodrigo@gmail.com>
2023-07-31 02:04:32 +0800

    Up

--- Hello.py
+++ Hello.py
@@ -1,1 +1,17 @@
-print("Hello, World")
+#Asking user for their name
+name = input ("What's your name? ")
+
+# print("Hello, ",name,"!")
+print("Hello,",name,"!") #Greet the user!
+print("Hello, " + name,"!") #Greet the user!
+
+# Printing options
+# print ("Hello, ", end="")
+# print (name)
+
+# Printing options
+# print ("Hello, ", sep=" ", end="") # Sep->separation
+# print (name)
+
+##Printing Double Quation (Escape char)
+print (F"Hello, \" {name} \"")
